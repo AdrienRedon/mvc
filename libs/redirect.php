db690fc10377190bdf6036130f2ac75f5a8c4c25
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2014-10-07 13:04:32 +0200

    Add redirect back with input

--- libs/redirect.php
+++ libs/redirect.php
@@ -2,9 +2,22 @@
 
 class Redirect
 {
+	protected $session;
+	
+	public function __construct(SessionInterface $session)
+	{
+		$this->session = $session;
+	}
+	
 	public function back()
 	{
 		header('Location: ' . $_SERVER['HTTP_REFERER']);
+	}
+	
+	public function backWithInput($input)
+	{
+		$this->session->set('input', $input);
+		$this->back();
 	}
 
 	public function to($url)
