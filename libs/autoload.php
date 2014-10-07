4c37ec36d9e0dfc8515009ecba7f03984e0dcf3b
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2014-10-07 08:27:50 +0200

    Add autoload

---
+++ libs/autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Interfaces
+ */
+
+require_once(ROOT . 'libs/interfaces/session_interface.php');
+
+
+/**
+ * Classes
+ */
+
+require_once(ROOT . 'libs/session.php');
+require_once(ROOT . 'libs/auth.php');
+require_once(ROOT . 'libs/flash.php');
+require_once(ROOT . 'libs/redirect.php');
+require_once(ROOT . 'libs/html.php');
+require_once(ROOT . 'libs/form.php');
