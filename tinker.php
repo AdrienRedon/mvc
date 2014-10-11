f87236ba9a02c47e19b16b9ae5af260368f49ac7
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2014-10-11 14:06:16 +0200

    add constructor when generating

--- tinker.php
+++ tinker.php
@@ -10,13 +10,30 @@ if($argv[1] == 'generate')
 {
     if($argv[2] == 'controller' && isset($argv[3]))
     {
-        $file = fopen("controllers/{$argv[3]}.php", 'w') or die("Impossible de generer le fichier");
-        fwrite($file, "<?php\n\nclass {$argv[3]} extends \Core\Controller\n{\n\tpublic function __construct()\n\t{\n\t\tparent::__construct();\n\t}\n}");
+        $filename = "controllers/{$argv[3]}.php";
+
+        if(!file_exists($filename))
+        {
+            $file = fopen($filename, 'w') or die("Impossible de generer le fichier");
+            fwrite($file, "<?php\n\nclass {$argv[3]} extends \Core\Controller\n{\n\tpublic function __construct()\n\t{\n\t\tparent::__construct();\n\t}\n}");
+        }
+        else
+        {
+            die("Le controller est deja present");
+        }
     }
     else if($argv[2] == 'model' && isset($argv[3]))
     {
-        $file = fopen("models/{$argv[3]}.php", 'w') or die("Impossible de generer le fichier");
-        fwrite($file, "<?php\n\nclass {$argv[3]} extends \Core\Model\n{\n\tpublic function __construct()\n\t{\n\t\tparent::__construct();\n\t\t\$this->table = \"table_name\";\n\t\t\$this->hidden = [];\n\t}\n}");
+        $filename = "models/{$argv[3]}.php";
+
+        if (!file_exists($filename)) {
+            $file = fopen($filename, 'w') or die("Impossible de generer le fichier");
+            fwrite($file, "<?php\n\nclass {$argv[3]} extends \Core\Model\n{\n\tpublic function __construct()\n\t{\n\t\tparent::__construct();\n\t\t\$this->table = \"table_name\";\n\t\t\$this->hidden = [];\n\t}\n}");
+        }
+        else
+        {
+            die("Le model est deja present");
+        }
     }
     else
     {
