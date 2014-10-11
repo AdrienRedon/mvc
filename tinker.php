950078a54d54f7c5356e73537d48a8adabd128a0
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2014-10-11 12:29:51 +0200

    Add generate model

--- tinker.php
+++ tinker.php
@@ -10,11 +10,16 @@ if($argv[1] == 'generate')
 {
     if($argv[2] == 'controller' && isset($argv[3]))
     {
-        $file = fopen("controllers/{$argv[3]}.php", "w") or die("Impossible d'ouvrir le fichier");
+        $file = fopen("controllers/{$argv[3]}.php", 'w') or die("Impossible de generer le fichier");
         fwrite($file, "<?php\n\nclass {$argv[3]} extends \Core\Controller\n{\n\t\n}");
+    }
+    else if($argv[2] == 'model' && isset($argv[3]))
+    {
+        $file = fopen("models/{$argv[3]}.php", 'w') or die("Impossible de generer le fichier");
+        fwrite($file, "<?php\n\nclass {$argv[3]} extends \Core\Model\n{\n\t\n}");
     }
     else
     {
-        echo 'Les aguments entrés ne sont pas corrects';
+        echo 'Les arguments entrés ne sont pas corrects';
     }
 }
