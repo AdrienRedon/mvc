c58a795821bf70f3536140cf155475ab77fedf30
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-03-01 15:22:05 +0100

    First commit of config

---
+++ config/config.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+	/**
+	 * Mode debug
+	 */
+	'debug' => true,
+
+	/**
+	 * Configuration de la base de données SQL
+	 */
+	'sql_host' => 'localhost',
+	'sql_base' => 'mvc',
+	'sql_login' => 'root',
+	'sql_password' => '',
+
+	/**
+	 * Configuration du controller et de la méthode à appeller par défaut
+	 */
+	'default_controller' => 'page',
+	'default_method' => 'index',
+	'default_args' => null,
+
+	/**
+	 * Configuration des vues
+	 */
+	'default_layout' => 'default',
+	'default_title' => 'MVC'
+
+];
