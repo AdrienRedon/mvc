cbee36e667a3ef93bb902bb85b83ba3ba05660b8
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-03-03 21:01:59 +0100

    Indentation problem

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -4,21 +4,21 @@ use \Core\Model;
 
 class PageController extends \Core\Controller
 {
-	protected $page;
-	protected $posts;
+    protected $page;
+    protected $posts;
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->page = Model::load('page');
-		$this->post = Model::load('post');
+    public function __construct()
+    {
+        parent::__construct();
+        $this->page = Model::load('page');
+        $this->post = Model::load('post');
         $this->user = Model::load('user');
-	}
+    }
 
-	public function index()
-	{
-		$user = $this->auth->user();
-		$content = $this->page->first()->content;
+    public function index()
+    {
+        $user = $this->auth->user();
+        $content = $this->page->first()->content;
         $posts = $this->user->first()->post;
 
         $last_post = $posts->orderBy('name', 'asc')->last('name');
@@ -29,13 +29,13 @@ class PageController extends \Core\Controller
         $new_post->user_id = $user->id;
         $new_post->save();
 
-		if($this->isAjax())
-		{
-			$this->view->json(compact('content'));
-		}
-		else
-		{
-			$this->view->render('index', compact('content', 'user', 'posts', 'last_post'));
-		}
-	}
+        if($this->isAjax())
+        {
+            $this->view->json(compact('content'));
+        }
+        else
+        {
+            $this->view->render('index', compact('content', 'user', 'posts', 'last_post'));
+        }
+    }
 }
