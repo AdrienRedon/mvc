f965fe321d3c752fff7b9e5c05e1a856f7503519
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-03-03 20:45:21 +0100

    Active record save, create and update

--- core/Model.php
+++ core/Model.php
@@ -6,141 +6,173 @@ use \Libs\Collection;
 
 class Model
 {
-	protected $table;
-	public $id;
-	protected $db;
-	
-	protected $fields;
+    protected $db;
+    
+    protected $table;
+    
+    public $id;
 
     /**
-     * Relationships
+     * List of the fillable fields
+     * @var array
+     */
+    protected $fields;
+
+    /**
+     * Hidden fields
+     */
+    protected $hidden = array();
+
+    /**
+     * Querying Relations
      */
     protected $has_one = array();
     protected $has_many = array();
     protected $belongs_to = array();
     protected $belongs_to_many = array();
 
-	/**
-	 * Hidden fields
-	 */
-	protected $hidden = array();
-
-	/**
-	 * Constructor
-	 */ 
-	public function __construct()
-	{
+    /**
+     * Constructor
+     */ 
+    public function __construct()
+    {
         $host = Config::getInstance()->get('sql_host');
         $base = Config::getInstance()->get('sql_base');
         $login = Config::getInstance()->get('sql_login');
         $password = Config::getInstance()->get('sql_password');
         
         $this->db = new Database($host, $base, $login, $password);
-	}
+    }
 
-	/**
-	 * Read the given fields of the current record from the database
-	 * @param $fields array
-	 */
-	public function read($fields = array())
-	{
-	    foreach($fields as $k => $field)
-		{
-			if(!in_array($field, $this->fields))
-			{
-				unset($fields[$k]);	
-			}
-		}
+    /**
+     * Read the given fields of the current record from the database
+     * @param $fields array
+     */
+    public function read($fields = array())
+    {
+        foreach($fields as $k => $field)
+        {
+            if(!in_array($field, $this->fields))
+            {
+                unset($fields[$k]); 
+            }
+        }
 
-		if(empty($fields))
-		{
-			$fields = '*';
-		} 
+        if(empty($fields))
+        {
+            $fields = '*';
+        } 
 
-		$sql = "SELECT $fields FROM {$this->table} WHERE id={$this->id}";
-		$data = $this->db->query($sql);
+        $sql = "SELECT $fields FROM {$this->table} WHERE id={$this->id}";
+        $data = $this->db->query($sql);
 
-		foreach ($data as $k => $v) 
-		{
-			$this->k = $v;
-		}
-	}
-	
-	/**
-	 * Create a record with the given data
-	 * @param $data array
-	 */ 
-	public function create($data = array())
-	{
-		$sql = "INSERT INTO {$this->table} (";
-		unset($data['id']);
-		foreach ($data as $k => $v) 
-		{
-			$sql .= "$k,";
-		}
-		$sql = substr($sql, 0, -1);
-		$sql .= ") VALUES (";
-		foreach ($data as $k => $v) 
-		{
-			$sql .= "'$v',";
-		}
-		$sql = substr($sql, 0, -1);
-		$sql .= ")";
-		$this->db->query($sql);
-	
-	    // set the id for the new record
-		$this->id = $this->db->getLastInsertedId();
-	}
-	
-	/**
-	 * Update the current record into the database
-	 * @param $data array
-	 */ 
-	public function update($data = array())
-	{
-	    $sql = "UPDATE {$this->table} SET ";
-	    if(empty($data))
-	    {
-	        foreach ($this->fields as $field) 
-		    {
-		    	if(isset($this->field) && $field != 'id')
-		    	{
-		    		$sql .= "$field = '$this->$field',";
-	    		}
-	    	}
-	    } 
+        foreach ($data as $k => $v) 
+        {
+            $this->k = $v;
+        }
+    }
+    
+    /**
+     * Create a record with the given data
+     * @param $data array
+     */ 
+    public function create($data = array())
+    {
+        $args = array();
+        $sql = "INSERT INTO {$this->table} (";
+        if(empty($data))
+        {
+            foreach ($this->fields as $field) 
+            {
+                if(isset($this->$field))
+                {
+                    $sql .= "$field, ";
+                }
+            }
+            $sql = substr($sql, 0, -2);
+            $sql .= ") VALUES (";
+            foreach ($this->fields as $field) 
+            {
+                if(isset($this->$field))
+                {
+                    $sql .= ":$field, ";
+                    $args[$field] = $this->$field;
+                }
+            }
+        }
         else
         {
-		    foreach ($data as $k => $v) 
-		    {
-		    	if(k != 'id')
-                {
-	                $sql .= "$k = '$v',";
-                }
-	    	}
-	    }
-	    
-	    $sql = substr($sql, 0, -1);
-	    $sql .= "WHERE id={$data['id']}";
-		
-		$this->db->query($sql);	
-	}
+            unset($data['id']);
+            foreach ($data as $k => $v) 
+            {
+                $sql .= "$k, ";
+            }
+            $sql = substr($sql, 0, -2);
+            $sql .= ") VALUES (";
+            foreach ($data as $field => $value) 
+            {
+                $sql .= ":$field, ";
+                $args[$field] = $value;
+            }
+        }
+        $sql = substr($sql, 0, -2);
+        $sql .= ")";
+        var_dump($sql);
+        var_dump($args);
 
-	/**
-	 * Save the given data
-	 * @param $data array
-	 */
-	public function save($data = array())
-	{
-		if(isset($this->id))
-		{
-			$this->update($data);
-		}
-		else
-		{
-			$this->create($data);
-		}
-	}
+        $this->id = $this->db->execute($sql, $args); // set the id for the new record
+        var_dump($this->id);
+    }
+    
+    /**
+     * Update the current record into the database
+     * @param $data array
+     */ 
+    public function update($data = array())
+    {
+        $sql = "UPDATE {$this->table} SET ";
+        if(empty($data))
+        {
+            foreach ($this->fields as $field) 
+            {
+                if(isset($this->field))
+                {
+                    $sql .= "$field = ?,";
+                }
+            }
+        } 
+        else
+        {
+            foreach ($data as $field => $value) 
+            {
+                if($k != 'id')
+                {
+                    $sql .= "$field = ?,";
+                }
+            }
+        }
+        
+        $sql = substr($sql, 0, -1);
+        $sql .= "WHERE id={$data['id']}";
+        
+        $this->db->execute($sql, $data); 
+    }
+
+    /**
+     * Save the given data
+     * @param $data array
+     */
+    public function save($data = array())
+    {
+        if(isset($this->id))
+        {
+            $this->update($data);
+        }
+        else
+        {
+            $this->create($data);
+        }
+    }
 
     /**
      * Return the line with the given conditions from the table
@@ -207,18 +239,18 @@ class Model
      * @param string $fields
      * @return mixed $result
      */
-	public function first($fields = '*')
-	{
-		$sql = "SELECT $fields FROM {$this->table}";
-		$result = $this->db->query($sql)->first();
-		
-		foreach ($this->hidden as $hidden) 
-		{
-			if(isset($result->$hidden))
-			{
-				unset($result->$hidden);
-			}
-		}
+    public function first($fields = '*')
+    {
+        $sql = "SELECT $fields FROM {$this->table}";
+        $result = $this->db->query($sql)->first();
+        
+        foreach ($this->hidden as $hidden) 
+        {
+            if(isset($result->$hidden))
+            {
+                unset($result->$hidden);
+            }
+        }
 
         $class = get_class($this);
         $object = new $class;
@@ -227,33 +259,33 @@ class Model
             $object->$attribute = $value;
         }
 
-		return $object;
-	}
+        return $object;
+    }
 
-	/**
-	 * Delete a line from the table
-	 * @param int $id
-	 */
-	public function delete($id = null)
-	{
-		if ($id == null) 
-		{
-			$id = $this->id;
-		}
-		$sql = "DELETE FROM {$this->table} WHERE id=$id";
-		$this->db->query($sql);
-	}
+    /**
+     * Delete a line from the table
+     * @param int $id
+     */
+    public function delete($id = null)
+    {
+        if ($id == null) 
+        {
+            $id = $this->id;
+        }
+        $sql = "DELETE FROM {$this->table} WHERE id=$id";
+        $this->db->query($sql);
+    }
 
     /**
      * Load the given model
      * @param string $name Name of the model to load
      * @return Object Model loaded
      */
-	static function load($name)
-	{
-		require_once(ROOT."models/".ucfirst($name).".php");
-		return new $name();
-	}
+    static function load($name)
+    {
+        require_once(ROOT."models/".ucfirst($name).".php");
+        return new $name();
+    }
 
 /**
  * Get and create (if first time) field from relationship between models
