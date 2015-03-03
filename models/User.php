7c83856bc3a2b27f9231d6b5ce1dc96f09cdd85c
Adrien REDON <AdrienRedon@users.noreply.github.com>
2015-03-03 14:29:08 +0100

    Update User.php

--- models/User.php
+++ models/User.php
@@ -3,6 +3,7 @@
 class User extends \Core\Model
 {
     protected $table = "users";
+    protected $fields = ['login', 'password'];
     protected $hidden = ['password'];
     protected $has_many = ['post'];
 }
