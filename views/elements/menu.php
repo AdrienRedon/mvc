8c7edcbd319d3211d231aa20fabdecc57343a283
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-03-10 11:08:40 +0100

    Add links to post

--- views/elements/menu.php
+++ views/elements/menu.php
@@ -1,5 +1,6 @@
 <nav class="menu">
 	<ul>
-		<li><?= $this->html->link('', 'Accueil') ?></li>
+        <li><?= $this->html->link('', 'Accueil') ?></li>
+		<li><?= $this->html->link('post', 'Post') ?></li>
 	</ul>
 </nav>
