17dd316395ada8a93f392b4edab3de33445652de
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-04-11 11:52:39 +0200

    Database interface

---
+++ libs/interfaces/DatabaseInterface.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace Libs\Interfaces;
+
+interface DatabaseInterface
+{
+    /**
+     * Get data from the database
+     * @param  string $sql  Request
+     * @param  array  $args Arguments for the request
+     * @return Collection   Data from database
+     */
+    public function query($sql, $args = array());
+
+    /**
+     * Execute an action to the database
+     * @param  string $sql  Request
+     * @param  array  $args Arguments for the request
+     * @return integer      Last inserted id
+     */
+    public function execute($sql, $args = array());
+}
