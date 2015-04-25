bfadf1b005c4a2ac0fbe6cc528194b47217c0940
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-04-25 13:58:42 +0200

    Use DIC to get Config

--- core/App.php
+++ core/App.php
@@ -6,14 +6,21 @@ use \Core\Config;
 
 class App
 {
+    /**
+     * Call method from the Dependency Injection Container 
+     */
     public static function __callStatic($method, $args = array())
     {
         return call_user_func_array(array(DIC::getInstance(), $method), $args);
     }
 
+    /**
+     * Associate all implementation classes to interfaces in the DIC as written in the config files
+     */
     public static function register()
     {
-        $providers = Config::getInstance()->get('providers');
+        $providers = SELF::get('Core\Config')->get('providers');
+
         foreach($providers as $interface => $class)
         {
             SELF::bind($interface, $class);
