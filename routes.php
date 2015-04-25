5f0e5c2bb82acb4809908f6f376f698d004f8a26
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-04-25 15:33:20 +0200

    List of routes

---
+++ routes.php
@@ -0,0 +1,21 @@
+<?php 
+
+use Core\Route;
+
+Route::get('/', 'PageController@index');
+
+Route::get('phpinfo', function() {
+    phpinfo();
+});
+
+Route::post('login', 'SessionController@login');
+Route::get('logout', 'SessionController@logout');
+
+Route::get('post', 'PostController@index');
+Route::get('post/{id}', 'PostController@show');
+
+Route::get('admin/post', 'Admin\PostController@index');
+Route::get('admin/post/create', 'Admin\PostController@create');
+Route::get('admin/post/update/{id}', 'Admin\PostController@update');
+Route::post('admin/post/save', 'Admin\PostController@save');
+Route::get('admin/post/delete', 'Admin\PostController@delete');
