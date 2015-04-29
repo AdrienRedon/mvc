d847f4dda0f1505040fb349c0fbb3d32aecd6106
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-04-29 19:25:58 +0200

    Use route resource

--- routes.php
+++ routes.php
@@ -14,9 +14,4 @@ Route::get('logout', 'SessionController@logout');
 Route::get('post', 'PostController@index');
 Route::get('post/{id}', 'PostController@show');
 
-Route::get('admin/post', 'Admin\PostController@index');
-Route::get('admin/post/create', 'Admin\PostController@create');
-Route::post('admin/post/store', 'Admin\PostController@store');
-Route::get('admin/post/{id}/update', 'Admin\PostController@update');
-Route::post('admin/post/{id}/save', 'Admin\PostController@save');
-Route::get('admin/post/{id}/delete', 'Admin\PostController@delete');
+Route::resource('admin/post', 'Admin\PostController', ['except' =>['show']]);
