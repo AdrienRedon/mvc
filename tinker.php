e7f0d1e5f4320256957f347207c6394de9ceee1d
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-05-09 18:18:03 +0200

    Add route when generating resource

--- tinker.php
+++ tinker.php
@@ -180,10 +180,28 @@ function createResource($name, $methods)
     createController($name, $methods);
     createModel($name);
     createMigration($name);
-    for($i = 0; $i < count($methods); $i++)
+    foreach($methods as $method)
     {
-        createView($name, $methods[$i]);
+        createView($name, $method);
     }
+
+    $routes = fopen('routes.php', 'a');
+    $str = "\n\n";
+    if($methods == ['index', 'show', 'create', 'store', 'edit', 'update', 'delete'])
+    {
+        $str .= "Route::resource('{$name}', '" . ucfirst($name) . "Controller')";
+    }
+    else {
+        $str .= "Route::resource('{$name}', '" . ucfirst($name) . "Controller', ['only' => [";
+        foreach($methods as $method)
+        {
+            $str .= "'$method', ";
+        }
+        $str = substr($str, 0, -2);
+        $str .= "]])";
+    }
+    fwrite($routes, $str);
+    fclose($routes);
 }
 
 function deleteResource($name, $methods)
