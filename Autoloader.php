aef6fc73ba099503fa2c08aa0d3fdec1880ec625
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-05-09 17:56:05 +0200

    doc autoloader methods

--- Autoloader.php
+++ Autoloader.php
@@ -2,12 +2,18 @@
 
 class Autoloader
 {
-
+    /**
+     * Register autoload method as autoloader
+     */
     public static function register()
     {
         spl_autoload_register(array(__CLASS__, 'autoload'));
     }
 
+    /**
+     * Autoload a class
+     * @param  string $class Name of the class
+     */
     protected static function autoload($class)
     {
         $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
