708799b498637dcbd2857d714acd702759eaab94
Adrien REDON <adrien.redon@etu.univ-lyon1.fr>
2015-05-15 09:16:47 +0200

    Delete controllers.js

--- public/js/controllers.js
+++
@@ -1,7 +0,0 @@
-// var controllers = angular.module('controllers', []);
-
-// controllers.controller('PageController', ['$scope', '$http'], function($scope, $http) {
-// 	$http.get('pages/index').success(function(data) {
-// 		$scope.content = data;
-// 	})
-// })
