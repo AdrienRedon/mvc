a941ccf0cbb4e2dcd4052803b74b63d0478662be
Adrien REDON <redon.adrien@gmail.com>
2015-08-13 18:00:01 +0200

    More tests for Controller !

--- test/Core/Controller/ControllerTest.php
+++ test/Core/Controller/ControllerTest.php
@@ -19,6 +19,22 @@ class ControllerTest extends PHPUnit_Framework_TestCase
 
     public function testCreateController()
     {
-        $this->assertEquals(call_user_func_array($this->controllerResolver->create('TestController@index'), array()), 'hello');
+        $this->assertEquals(call_user_func_array($this->controllerResolver->getAction('TestController@index'), array()), 'hello');
+    }
+
+    /**
+     * @expectedException App\Core\DependencyInjection\Exception\ServiceNotFoundException
+     */
+    public function testTrowsExceptionServiceNotFound()
+    {
+        $this->controllerResolver->getAction('NotFoundController@index');
+    }
+
+    /**
+     * @expectedException App\Core\Controller\Exception\MethodNotFoundException
+     */
+    public function testThrowsExceptionMethodNotFound()
+    {
+        $this->controllerResolver->getAction('TestController@notFoundMethod');
     }
 }
