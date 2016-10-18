5c6f448ae76c6bcd08718fd55b84b67c36cebf6c
Adrien REDON <AdrienRedon@users.noreply.github.com>
2016-10-18 15:44:42 +0200

    Create Flash.php

---
+++ app/Libs/Flash.php
@@ -0,0 +1,22 @@
+<?php 
+namespace App\Libs;
+use App\Libs\SessionInterface;
+class Flash
+{
+    protected $session;
+    const KEY = 'flash';
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+    public function set($msg)
+    {
+        $this->session->set(self::KEY, $msg);
+    }
+    public function get()
+    {
+        $flash = $this->session->get(self::KEY);
+        $this->session->destroy(self::KEY);
+        return $flash;
+    }
+}
