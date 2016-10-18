ceccd5cef9de849f8e945e0e1727940acb56eec9
Adrien REDON <AdrienRedon@users.noreply.github.com>
2016-10-18 15:45:54 +0200

    Update Controller.php

--- app/Core/Controller/Controller.php
+++ app/Core/Controller/Controller.php
@@ -1,14 +1,34 @@
 <?php 
-
 namespace App\Core\Controller;
-
 use App\Core\DependencyInjection\ContainerAware;
 use App\Core\DependencyInjection\ContainerInterface;
-
+use App\Libs\Redirection;
+use App\Libs\Flash;
 class Controller extends ContainerAware
 {
-    public function __construct(ContainerInterface $container = null)
+    protected $view;
+    protected $redirect;
+    /**
+     * In order to resolve Model
+     * @var ModelResolver
+     */
+    protected $model;
+    protected $flash;
+    public function __construct(ContainerInterface $container)
     {
         $this->setContainer($container);
+        $this->view = $this->container->resolve('View');
+        $this->redirect = $this->container->resolve('Redirection');
+        $this->view->setDirectoryPath('app/View/');
+        $this->model = $this->container->resolve('ModelResolver');
+        $this->flash = $this->container->resolve('Flash');
+    }
+    /**
+     * Check if the request is an AJAX request
+     * @return boolean [description]
+     */
+    public function isAjax()
+    {
+        return strtolower(filter_input(INPUT_SERVER, 'HTTP_X_REQUESTED_WITH')) === 'xmlhttprequest';
     }
 }
