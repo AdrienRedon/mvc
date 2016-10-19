5fa55e2dcaa8fb699a45d888e216c4041e45d71d
Adrien REDON <AdrienRedon@users.noreply.github.com>
2016-10-19 10:06:17 +0200

    Create Session.php

---
+++ app/Libs/Session.php
@@ -0,0 +1,25 @@
+<?php 
+namespace App\Libs;
+use App\Libs\SessionInterface;
+class Session implements SessionInterface
+{
+    public function __construct()
+    {
+        if (! isset($_SESSION))
+        {
+            session_start();
+        }
+    }
+    public function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+    public function get($key)
+    {
+        return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+    }
+    public function destroy($key)
+    {
+        unset($_SESSION[$key]);
+    }
+}
