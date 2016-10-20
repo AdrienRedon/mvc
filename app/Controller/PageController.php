3eb08095ce4b70381ac453e8114c34c198fcb4ee
Adrien REDON <redon.adrien@gmail.com>
2016-10-20 14:41:32 +0200

    Add PageController

---
+++ app/Controller/PageController.php
@@ -0,0 +1,12 @@
+<?php 
+namespace App\Controller;
+
+use App\Core\Controller\Controller;
+
+class PageController extends Controller 
+{
+    public function index() 
+    {
+        return 'Hello World!';
+    }
+}
